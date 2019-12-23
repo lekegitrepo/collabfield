1f9eb2a3250e3a3334c23c8d9fab2226281c329c
lekegitrepo <lekedevrepo@gmail.com>
2019-12-23 10:48:18 -0600

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens - add javascript code in the webpack folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("posts/style")
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
