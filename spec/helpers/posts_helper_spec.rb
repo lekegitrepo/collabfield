7ec5dfe70e6f328f8876a5080f4cf7d0ba54cd3a
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-03-03 09:15:23 -0600

    add spec for contact user helper

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe PostsHelper, type: :helper do
   context '#create_new_post_partial_path' do
     it "returns a signed_in partial's path" do
       helper.stub(:user_signed_in?).and_return(true)
-      expect(helper.create_new_post_partial_path). to (
+      expect(helper.create_new_post_partial_path). to(
         eq 'posts/branch/create_new_post/signed_in'
       )
     end
 
     it "returns a signed_in partial's path" do
       helper.stub(:user_signed_in?).and_return(false)
-      expect(helper.create_new_post_partial_path). to (
+      expect(helper.create_new_post_partial_path). to(
         eq 'posts/branch/create_new_post/not_signed_in'
       )
     end
@@ -21,14 +21,14 @@ RSpec.describe PostsHelper, type: :helper do
   context '#all_categories_button_partial_path' do
     it "returns an all_selected partial's path" do
       controller.params[:category] = ''
-      expect(helper.all_categories_button_partial_path).to (
+      expect(helper.all_categories_button_partial_path).to(
         eq 'posts/branch/categories/all_selected'
       )
     end
 
     it "returns an all_not_selected partial's path" do
       controller.params[:category] = 'category'
-      expect(helper.all_categories_button_partial_path).to (
+      expect(helper.all_categories_button_partial_path).to(
         eq 'posts/branch/categories/all_not_selected'
       )
     end
@@ -37,14 +37,14 @@ RSpec.describe PostsHelper, type: :helper do
   context '#no_posts_partial_path' do
     it "returns a no_posts partial's path" do
       assign(:posts, [])
-      expect(helper.no_posts_partial_path).to (
+      expect(helper.no_posts_partial_path).to(
         eq 'posts/branch/no_posts'
       )
     end
 
     it "returns an empty partial's path" do
       assign(:posts, [1])
-      expect(helper.no_posts_partial_path).to (
+      expect(helper.no_posts_partial_path).to(
         eq 'shared/empty_partial'
       )
     end
@@ -53,14 +53,14 @@ RSpec.describe PostsHelper, type: :helper do
   context '#post_format_partial_path' do
     it "returns a home_page partial's path" do
       helper.stub(:current_page?).and_return(true)
-      expect(helper.post_format_partial_path).to (
+      expect(helper.post_format_partial_path).to(
         eq 'posts/post/home_page'
       )
     end
 
     it "returns a branch_page partial's path" do
       helper.stub(:current_page?).and_return(false)
-      expect(helper.post_format_partial_path).to (
+      expect(helper.post_format_partial_path).to(
         eq 'posts/post/branch_page'
       )
     end
@@ -80,6 +80,37 @@ RSpec.describe PostsHelper, type: :helper do
       assign(:posts, posts)
       expect(helper.update_pagination_partial_path).to(
         eq 'posts/posts_pagination_page/remove_pagination'
+      )
+    end
+  end
+
+  context '#contact_user_partial_path' do
+    before(:each) do
+      @current_user = create(:user, id: 1)
+      helper.stub(:current_user).and_return(@current_user)
+    end
+
+    it "returns a contact_user partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: create(:user, id: 2).id))
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/contact_user' 
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: @current_user.id))
+
+      expect(helper.contact_user_partial_path).to(
+        eq 'shared/empty_partial'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(false)
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/login_required'
       )
     end
   end
